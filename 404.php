dbe0964d4615c3dd9d53c4535979173738e463dd
Patrick <patrick.leisinger@gmail.com>
2020-02-19 19:19:22 +0100

    SP7-35 [ADD] add 404 error page
    - optimize search
    - own searchform template

--- 404.php
+++ 404.php
@@ -8,47 +8,7 @@
  */
 
 get_header();
-?>
 
-  <section class="error-404 not-found">
-    <header class="page-header">
-      <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'soapatrickseven' ); ?></h1>
-    </header>
+  get_template_part( 'template-parts/content', 'none' );
 
-    <div class="page-content">
-      <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'soapatrickseven' ); ?></p>
-
-      <?php
-      get_search_form();
-
-      the_widget( 'WP_Widget_Recent_Posts' );
-      ?>
-
-      <div class="widget widget_categories">
-        <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'soapatrickseven' ); ?></h2>
-        <ul>
-          <?php
-          wp_list_categories( array(
-            'orderby'    => 'count',
-            'order'      => 'DESC',
-            'show_count' => 1,
-            'title_li'   => '',
-            'number'     => 10,
-          ) );
-          ?>
-        </ul>
-      </div>
-
-      <?php
-      /* translators: %1$s: smiley */
-      $soapatrickseven_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'soapatrickseven' ), convert_smilies( ':)' ) ) . '</p>';
-      the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$soapatrickseven_archive_content" );
-
-      the_widget( 'WP_Widget_Tag_Cloud' );
-      ?>
-
-    </div>
-  </section>
-
-<?php
 get_footer();
--- layout-parts/search.php
+++ layout-parts/search.php
@@ -8,11 +8,7 @@
 ?>
 
 <div id="search-collapse">
-  <div class="container">
-    <form class="search-form" action="<?php echo home_url( '/' ); ?>" method="get">
-      <label class="grid" for="search-collapse--input">
-        <input type="text" name="s" id="search-collapse--input" value="<?php the_search_query(); ?>" placeholder="<?php esc_html_e( 'Find Stuff...', 'soapatrickseven' ); ?>" aria-label="<?php esc_html_e( 'Find Stuff...', 'soapatrickseven' ); ?>"">
-      </label>
-    </form>
+  <div class="grid">
+    <?php get_search_form(); ?>
   </div>
 </div>
