e82700932454365068c29ac437c7d68e0c73348c
Patrick <patrick.leisinger@gmail.com>
2020-02-24 20:13:23 +0100

    SP7-38 [UPDATE] move everything to one functions file

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@
 require get_template_directory() . '/inc/theme-setup.php';
 require get_template_directory() . '/inc/enqueue-scripts.php';
 require get_template_directory() . '/inc/customizer.php';
-require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/custom-post-type.php';
 require get_template_directory() . '/inc/custom-taxonomies.php';
