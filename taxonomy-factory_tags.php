2a65b8a465605558bb63385bc9b53b92fef8c03a
Patrick <patrick.leisinger@gmail.com>
2020-02-26 19:00:47 +0100

    SP7-59 [FIX] fix template name request after rename

--- taxonomy-factory_tags.php
+++ taxonomy-factory_tags.php
@@ -51,7 +51,7 @@ get_header(); ?>
         <?php
           if ( have_posts() ) :
             while ( have_posts() ) : the_post();
-              get_template_part( 'template-parts/content', 'factory' );
+              get_template_part( 'template-parts/content', 'featured-full' );
             endwhile;
           endif;
         ?>
